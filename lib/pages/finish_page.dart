2e8c7d60e839e2c4ffedad6cca100f4028aa1b20
rob1juan <robinjuan59@gmail.com>
2023-04-11 15:34:50 +0200

    Ajout du message copié dans le presse papier

--- lib/pages/finish_page.dart
+++ lib/pages/finish_page.dart
@@ -1,8 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:masyu_game/Theme/Buttons.dart';
 import 'package:masyu_game/Theme/Layout.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:clipboard/clipboard.dart';
 
 class FinishPage extends StatelessWidget {
+  final TextEditingController _textController = TextEditingController();
+
+  void _shareScore(BuildContext context) {
+    String playerName = _textController.text;
+
+    if (playerName.isEmpty) {
+      Fluttertoast.showToast(
+          msg: "Vous ne pouvez pas partager le score sans nom de joueur.",
+          toastLength: Toast.LENGTH_LONG,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0);
+    } else {
+      String message =
+          "Difficulté: Moyen\nGrille: 2\nTemps: 2min24\nEssaye de me battre ;)";
+      FlutterClipboard.copy(message).then((value) => Fluttertoast.showToast(
+          msg: "Message copié dans le presse-papier avec succès!",
+          toastLength: Toast.LENGTH_LONG,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.green,
+          textColor: Colors.white,
+          fontSize: 16.0));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,23 +84,23 @@ class FinishPage extends StatelessWidget {
                     color: Color.fromRGBO(255, 255, 255, 0.137),
                   ),
                   child: TextField(
-                      decoration: const InputDecoration(
-                        border: InputBorder.none,
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide.none,
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide.none,
-                        ),
+                    controller: _textController,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide.none,
                       ),
-                      autofocus: false,
-                      style: TextStyle(
-                        color: Color.fromRGBO(255, 255, 255, 1),
-                      )),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                    style: const TextStyle(color: Colors.white),
+                    autofocus: false,
+                  ),
                 ),
                 const SizedBox(height: 20),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () => _shareScore(context),
                   child: const Text('PARTAGER'),
                   style: ShareButton,
                 ),
