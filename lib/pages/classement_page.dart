46a457bc24a696bcdf766e6c23c2a8a2c5956674
Aurélien <aurelien.dufour257@gmail.com>
2023-04-13 18:36:31 +0200

    try to make classement responsive (to test on different sizes of screen)

--- lib/pages/classement_page.dart
+++ lib/pages/classement_page.dart
@@ -28,26 +28,29 @@ class classement_page extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Récupérer la taille de l'écran
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    // Définir des tailles responsives pour les espacements
+    double verticalSpacing = screenHeight * 0.025;
+    double topSpacing = screenHeight * 0.05;
+
     return Scaffold(
       body: Stack(
         children: BuildBasicLayout([
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.07,
-          ),
+          SizedBox(height: topSpacing * 0.75),
           const Text(
             "CLASSEMENT",
             style: TextStyle(
                 fontSize: 23, color: Colors.white, fontWeight: FontWeight.w600),
           ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.03,
-          ),
           Container(
-            width: MediaQuery.of(context).size.width * 0.8,
+            width: screenWidth * 0.8,
             child:
                 Column(mainAxisAlignment: MainAxisAlignment.center, children: [
               Container(
-                height: 60 * 5,
+                height: verticalSpacing * 15,
                 child: ScrollConfiguration(
                   behavior: ScrollConfiguration.of(context)
                       .copyWith(scrollbars: false),
@@ -55,7 +58,7 @@ class classement_page extends StatelessWidget {
                       itemCount:
                           players.length - 1, // nombre d'éléments dans la liste
                       itemBuilder: (context, index) =>
-                          ScoreRow(context, index, players[index], false)),
+                          ScoreRow(context, index + 1, players[index], false)),
                 ),
               ),
               Divider(
@@ -83,9 +86,11 @@ class classement_page extends StatelessWidget {
 
   Widget ScoreRow(
       BuildContext context, int index, ScoreBoardEntry score, bool isActual) {
+        double screenHeight = MediaQuery.of(context).size.height;
+        double verticalSpacing = screenHeight * 0.025;
     return Column(children: [
       Container(
-          height: 54,
+          height: verticalSpacing * 2.4,
           decoration: BoxDecoration(
             color: isActual
                 ? Color(0xFFF19BFF).withOpacity(0.35)
